92e94b1db0057d3d629d673df4c3c9534e7d9ec2
shrideepseek <sayking300@gmail.com>
2025-06-20 13:24:00 +0530

    app.py

--- app.py
+++ app.py
@@ -26,4 +26,5 @@ def webhook():
     return "Method Not Allowed", 405
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
